2f8fa7a28ce6b29ace8740f902d248633ab4d4da
Bethany Fulcher <fulcherbethany@gmail.com>
2021-05-18 13:22:19 -0700

    added blog model

---
+++ models/Blog.js
@@ -0,0 +1,34 @@
+const moment = require('moment')
+
+module.exports = function(sequelize, DataTypes) {
+    var Blog = sequelize.define('Blog', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        DateTime: {
+            type: DataTypes.DATEONLY,
+            get: function() {
+                return moment(this.getDataValue('DateTime')).format('DD.MM.YYYY')
+            }
+        },
+        likes: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    });
+
+    Blog.associate = function(models) {
+        Blog.belongsTo(models.User)
+    };
+
+    return Blog;
+}
