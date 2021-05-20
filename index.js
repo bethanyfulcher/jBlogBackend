3f9cbe40e268117ee0d9fb2b3addb08cb64cca7e
Bethany Fulcher <fulcherbethany@gmail.com>
2021-05-19 17:08:16 -0700

    reverted back to original

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+var express = require('express');
 const cors = require("cors");
 // Sets up the Express App
-// ========================================================
+// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
+
 
 // Requiring our models for syncing
 var db = require('./models');
@@ -12,22 +13,23 @@ var db = require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// DEVELOPMENT
+//DEVELOPMENT
 app.use(cors())
-// PRODUCTION
+//PRODUCTION
 // app.use(cors({
-//     origin:["INSERT FRONTEND DEPLOYED URL"]
+//     origin:["https://nov2020fishfront.herokuapp.com"]
 // }))
+
 
 const userRoutes = require("./controllers/user");
 app.use(userRoutes);
-const blogRoutes = require("./controllers/blog");
-app.use("/api/blogs",blogRoutes);
-// const fishRoutes = require("./controllers/fish");
-// app.use("/api/fishes", fishRoutes);
+const tankRoutes = require("./controllers/tank");
+app.use("/api/tanks",tankRoutes);
+const fishRoutes = require("./controllers/fish");
+app.use("/api/fishes",fishRoutes);
 
 db.sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
-        console.log('App listening on PORT ' + PORT)
-    })
-})
+    console.log('App listening on PORT ' + PORT);
+    });
+});
